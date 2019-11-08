4c22152fef93e89d198b9e95edde5c4d8e010ff0
georgiyfarina01 <georgiy.farina@samtrevano.ch>
2019-11-08 16:25:09 +0100

    Codice sorgente aggiornato

--- Source/SnowflakeGenerator/src/snowflakegenerator/StartMenu.java
+++ Source/SnowflakeGenerator/src/snowflakegenerator/StartMenu.java
@@ -33,9 +33,9 @@ public class StartMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        titolo = new javax.swing.JLabel();
+        bottoneApri = new javax.swing.JButton();
+        bottoneCrea = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 204, 204));
@@ -43,21 +43,21 @@ public class StartMenu extends javax.swing.JFrame {
         setName("StartMenu"); // NOI18N
         setPreferredSize(new java.awt.Dimension(512, 384));
 
-        jLabel1.setFont(new java.awt.Font("Century Gothic", 0, 36)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Snowflake Generator");
+        titolo.setFont(new java.awt.Font("Century Gothic", 0, 36)); // NOI18N
+        titolo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        titolo.setText("Snowflake Generator");
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setLabel("Apri");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        bottoneApri.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        bottoneApri.setLabel("Apri");
+        bottoneApri.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 apriProgetto(evt);
             }
         });
 
-        jButton2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton2.setLabel("Crea");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        bottoneCrea.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        bottoneCrea.setLabel("Crea");
+        bottoneCrea.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 creaProgetto(evt);
             }
@@ -71,26 +71,26 @@ public class StartMenu extends javax.swing.JFrame {
                 .addContainerGap(327, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(bottoneApri, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(70, 70, 70)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jLabel1))
+                        .addComponent(bottoneCrea, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(titolo))
                 .addContainerGap(327, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(titolo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(91, 91, 91)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(bottoneApri, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bottoneCrea, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(487, Short.MAX_VALUE))
         );
 
-        jButton1.getAccessibleContext().setAccessibleName("apri");
-        jButton2.getAccessibleContext().setAccessibleName("CreaButton");
+        bottoneApri.getAccessibleContext().setAccessibleName("apri");
+        bottoneCrea.getAccessibleContext().setAccessibleName("CreaButton");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -152,8 +152,8 @@ public class StartMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton bottoneApri;
+    private javax.swing.JButton bottoneCrea;
+    private javax.swing.JLabel titolo;
     // End of variables declaration//GEN-END:variables
 }
